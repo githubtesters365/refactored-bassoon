30896de0d1914bfade7279bcb1971e9a9b534072
githubtesters365 <80769171+githubtesters365@users.noreply.github.com>
2021-03-30 18:04:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,24 +95,6 @@ with col3:
 st.image(image, caption='The Moon')
 
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-         hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
-
-
 video_file = open('lol.mp4', 'rb')
 video_bytes = video_file.read()
 
